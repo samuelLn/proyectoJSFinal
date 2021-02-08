c9d34201708746bc410d5939c0eef9bea9df3979
samuelLn <44329599+samuelLn@users.noreply.github.com>
2021-02-07 23:04:37 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 //Clase 1
 class PokemonBasico {
-    
- //Variables base
+  //Variables base
   nombre = "";
   tipo = "";
   nivel = 0;
-  ciudad = "Planta";    
-    
-constructor({nombre, tipo, nivel, ciudad}) {
+  ciudad = "Planta";
+
+  constructor({ nombre, tipo, nivel, ciudad }) {
     this.nombre = nombre;
     this.tipo = tipo;
     this.nivel = nivel;
@@ -27,8 +26,6 @@ constructor({nombre, tipo, nivel, ciudad}) {
   setCiudad = nuevaCiudad => (this.ciudad = nuevaCiudad);
 }
 
-
-
 //LLamadas a las clases1
 //const pokemonBasico = new PokemonBasico("Bulbasaur", "Planta", 20, "Ciudad Verde");
 //pokemonBasico;
@@ -37,21 +34,19 @@ constructor({nombre, tipo, nivel, ciudad}) {
 //pokemonBasico.setCiudad("Santiago");
 //pokemonBasico.getCiudad();
 
-
 //Clase 2 HERENCIA-1
 class PokemonMedio extends PokemonBasico {
+  //Nueva herencia
+  nombreEntrenador = "";
 
-//Nueva herencia
-nombreEntrenador = "";
+  constructor({ nombreEntrenador, nombre, tipo, nivel, ciudad }) {
+    super({ nombre, tipo, nivel, ciudad });
+    this.nombreEntrenador = nombreEntrenador;
+  }
 
-constructor({nombreEntrenador, nombre, tipo, nivel, ciudad }) {
-super({nombre, tipo, nivel, ciudad});
-this.nombreEntrenador = nombreEntrenador;   
-}
-
-getNombreEntrenador = () => this.nombreEntrenador;
-setNombreEntrenador = nuevoNombreEntrenador => (this.nombreEntrenador = nuevoNombreEntrenador);
-
+  getNombreEntrenador = () => this.nombreEntrenador;
+  setNombreEntrenador = nuevoNombreEntrenador =>
+    (this.nombreEntrenador = nuevoNombreEntrenador);
 }
 
 //LLamadas a las clase 2
@@ -61,57 +56,113 @@ setNombreEntrenador = nuevoNombreEntrenador => (this.nombreEntrenador = nuevoNom
 //pokemonA.getNombre()
 //pokemonA.getNombreEntrenador()
 
-
-//Clase 3 HERENCIA-2 
+//Clase 3 HERENCIA-2
 
 class PokemonLegendario extends PokemonMedio {
+  //Nueva herencia
+  nombrePokemonLegendario = "";
 
-//Nueva herencia
-nombrePokemonLegendario = "";
-    
-constructor({nombrePokemonLegendario, nombreEntrenador, nombre, tipo, nivel, ciudad }) {
-super({nombreEntrenador, nombre, tipo, nivel, ciudad});
-this.nombrePokemonLegendario = nombrePokemonLegendario;   
+  constructor({
+    nombrePokemonLegendario,
+    nombreEntrenador,
+    nombre,
+    tipo,
+    nivel,
+    ciudad
+  }) {
+    super({ nombreEntrenador, nombre, tipo, nivel, ciudad });
+    this.nombrePokemonLegendario = nombrePokemonLegendario;
+  }
+
+  getNombrePokemonLegendario = () => this.nombrePokemonLegendario;
+  setNombrePokemonLegendario = nuevoNombrePokemonLegendario =>
+    (this.nombrePokemonLegendario = nombrePokemonLegendario);
 }
-    
-getNombrePokemonLegendario = () => this.nombrePokemonLegendario;
-setNombrePokemonLegendario = nuevoNombrePokemonLegendario => (this.nombrePokemonLegendario = nombrePokemonLegendario);
-    
-}
-  
+
 //LLamadas a las clase 3
 //const pokemonL = new PokemonLegendario ({nombrePokemonLegendario:'Butterfree Gigamax', nombreEntrenador:'Jesy', nombre:' Butterfree', tipo:'Bicho', nivel: 40, ciudad:'Ciudad Verde'})
 
 // LLAMADAS METODOS
 //pokemonL.getNombrePokemonLegendario()
-//pokemonL.setNombrePokemonLegendario()--- este metodo tiene un bugs que no pude solucionar
-
-
-
+//pokemonL.setNombrePokemonLegendario()
 
 //LLAMADA API PUBLILCA
 
-
-const Url = 'https://pokeapi.co/api/v2/pokemon/1/' 
+const Url = "https://pokeapi.co/api/v2/pokemon/1/";
 fetch(Url)
-
-.then(response => response.json () )
-.then(data => { 
-    
-    let ContentDatos = document.getElementById('ContentDatos')
-    ContentDatos.innerHTML =`
+  .then(response => response.json())
+  .then(data => {
+    let ContentDatos = document.getElementById("ContentDatos");
+    ContentDatos.innerHTML = `
     <h1 class="display-1"> ${data.name}</h1>
     <img class="img-fluid" src='${data.sprites.front_default}' alt="">
     <p class="Nombbre1"> ${data.order}</p>   
      `;
-    
-    
 
-console.log(data)
+    console.log(data);
+  })
 
-})
+  .catch(err => console.log(err));
 
-.catch(err=>console.log(err))
+///////////////// METODOS DE FILTROS PARA ARRAY///////////////////////////
+// Para poder ejeucutar los metodos de Array, utilice el archivo json de muestra
+
+const filterOrders = () => {
+  console.time("orders");
+  const result = sandwichOrders.filter(
+    sandwichOrder => sandwichOrder.bread === "bagette"
+  );
+  console.timeEnd("orders");
+  console.log(result);
+};
+
+// METODO PARA TIPO DE PAN
+const filterOrdersByBread = filter =>
+  sandwichOrders.filter(sandwichOrder => sandwichOrder.bread === filter);
+
+var itemBuquedaPan = "marraqueta";
+//filterOrdersByBread(itemBuqueda);
+
+// METODO PARA BUSCAR TIPO DE PROTEINAS
+const filterOrdersByProtein = filter =>
+  sandwichOrders.filter(sandwichOrder => sandwichOrder.protein === filter);
+
+var itemBuquedaProteina = "churrasco";
+//filterOrdersByProtein(itemBuquedaProteina);
+
+// METODO PARA BUSCAR FECHAS DE LA ORDEN
+const filterOrdersByOrden = filter =>
+  sandwichOrders.filter(sandwichOrder => sandwichOrder.ordered === filter);
+
+var itemBuquedaOrden = "05-10-2020";
+//filterOrdersByOrden(itemBuquedaOrden);
+
+// METODO MAP
+
+const getSandwichOrdersId = () =>
+  sandwichOrders.map(sandwichOrder => sandwichOrder.id);
+//getSandwichOrdersId();
+
+
+const getSandwichOrdersProtein = () =>
+  sandwichOrders.map(sandwichOrder => sandwichOrder.protein);
+//getSandwichOrdersProtein();
+
+
+
+// METODO FIND
+const findSandwichById = (id) => sandwichOrders.find (
+(sandwichOrder) => sandwichOrder.id === id
+)   
+
+//findSandwichById()
+
+
+const findSandwichByProtein = (protein) => sandwichOrders.find (
+(sandwichOrder) => sandwichOrder.protein === protein
+)
+
+//findSandwichByProtein("hamburguesa lentejas")
 
 
 
@@ -120,9 +171,3 @@ console.log(data)
 
 
 
-
-
-
-    
-    
-  
